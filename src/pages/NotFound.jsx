8bd2aa49ee380a25e716b8e5d5664a4d02ab1b5c
Abduraxim Berdiqobilov <abduraximberdiqobilov@Abduraxims-MacBook-Air.local>
2024-12-02 23:07:20 +0500

    notfound photo fixed

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
 
-import foto from "../assets/notfound.jpg"
+import foto from "../../public/notfound.jpg"
 
 const NotFound = () => {
     const navigate = useNavigate()
